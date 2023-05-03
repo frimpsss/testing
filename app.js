3397c5691d4fb9619d3851ed6f308f3ad316b35d
frimpsss <afrimpong031@gmail.com>
2023-05-03 21:47:25 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function showWidget() {
+  var widgetContainer = document.createElement("div");
+  widgetContainer.innerHTML = `
+    <div id="my-widget">
+      <h2>My Widget</h2>
+      <p>This is my widget. It's really cool!</p>
+    </div>
+  `;
+  document.body.appendChild(widgetContainer);
+}
