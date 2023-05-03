641b05d536db93d7b2badbe767665b9ed947862f
frimpsss <afrimpong031@gmail.com>
2023-05-03 22:17:22 +0000

    test

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ function showWidget() {
   var widgetContainer = document.createElement("div");
   widgetContainer.innerHTML = `
     <div id="my-widget">
+        <style>
+        #my-widget{
+            background: green;
+        }
+        <style>
       <h2>My Widget</h2>
       <p>This is my widget. It's really cool!</p>
     </div>
   `;
   document.body.appendChild(widgetContainer);
 }
+
+
