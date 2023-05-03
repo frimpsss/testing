50a09d4f38fcdb0016e6e291f228594bdfed678b
frimpsss <afrimpong031@gmail.com>
2023-05-03 22:21:34 +0000

    test 2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function showWidget() {
   widgetContainer.innerHTML = `
     <div id="my-widget" style="backround: green;">
       <h2>My Widget</h2>
-      <p>This is my widget. It's really cool!</p>
+      <p>This is my widget. It's not really cool!</p>
     </div>
   `;
   document.body.appendChild(widgetContainer);
