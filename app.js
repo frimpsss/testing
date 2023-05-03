710454f41d3148a0d6042eb2bacff26b166a0276
frimpsss <afrimpong031@gmail.com>
2023-05-03 22:19:54 +0000

    test

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 function showWidget() {
   var widgetContainer = document.createElement("div");
   widgetContainer.innerHTML = `
-    <div id="my-widget">
-        <style>
-        #my-widget{
-            background: green;
-        }
-        <style>
+    <div id="my-widget" style="backround: green;">
       <h2>My Widget</h2>
       <p>This is my widget. It's really cool!</p>
     </div>
